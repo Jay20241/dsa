1de17dda602955a6cf15f2bafad0031206b9a652
Jay20241 <167299716+Jay20241@users.noreply.github.com>
2025-06-07 09:02:54 +0000

    bs in infinite or not fixed size array

--- tut14.java
+++ tut14.java
@@ -39,5 +39,33 @@ public class BinarySearch{
         }
         return -1;
     }
+
+
+    //BS in Infinite array
+    static int ans(int[] arr, int target){
+        int start = 0;
+        int end = 1;
+
+        while(target > arr[end]){
+            int temp = end + 1;
+            end = end + (end - start + 1)*2;
+            start = temp;
+        }
+
+        return BSInInfinite(arr, target, start, end);
+    }
+    static int BSInInfinite(int[] arr, int target, int start, int end){
+        while(start <= end){
+            int mid = start + ((end - start)/2);
+            if(target < arr[mid]){
+                end = mid - 1;
+            }else if(target > arr[mid]){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
 
