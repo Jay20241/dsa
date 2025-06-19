c7335dbc0b57243e71a3abb72f5b4f987608feec
Jay20241 <167299716+Jay20241@users.noreply.github.com>
2025-06-19 11:45:11 +0000

    SubSeq, SubSet with Duplicates

--- tut32.java
+++ tut32.java
@@ -1,6 +1,14 @@
 public class Tut32{
     psvm(){
-        skipA("", "baccad")
+        skipA("", "baccad");
+
+        subseq("", "abc");
+
+        //print ascii value of char
+        char ch = 'a';
+        System.out.println(ch+0); //97
+        System.out.println((char)(ch+0)); //a
+
     }
 
     static void skipA(String ans, String str){
@@ -18,7 +26,7 @@ public class Tut32{
         }
     }
 
-    static String skipA(String str){
+    static String skipA_return(String str){
         if(str.isEmpty()){
             return "";
         }
@@ -26,9 +34,9 @@ public class Tut32{
         char ch = str.chatAt(0);
 
         if(ch=='a'){
-            return skipA(str.substring(1));
+            return skipA_return(str.substring(1));
         }else{
-            return ch + skipA(str.substring(1));
+            return ch + skipA_return(str.substring(1));
         }
     }
 
@@ -60,5 +68,128 @@ public class Tut32{
         }else{
             return str.charAt(0) + skipApp_NotApple(str.substring(1));
         }
+    }
+
+    // Subsequence is for String 
+    // Subset is for Arrays
+
+    // p - processed = answer
+    // up - unprocessed = original str
+    //String is immutable. => Many reference variables pointing to same obj.
+    static void subseq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subseq(p + ch, up.substring(1));
+        subseq(p, up.substring(1));
+    }
+
+
+
+    //Return all subseq in arraylist
+    static ArrayList<String> subseq_return(String p, String up){
+        if(up.isEmpty()){
+           ArrayList<String> list = new ArrayList<>();
+           list.add(p);
+           return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> with = subseq_return(p + ch, up.substring(1));
+        ArrayList<String> without = subseq_return(p, up.substring(1));
+
+        with.addAll(without);
+        return with;
+    }
+
+    //print ascii value
+    /**
+     * 3 choices - Take char, Take Ascii value, Ignore
+    */
+   static void subseq_ascii(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subseq_ascii(p + ch, up.substring(1));
+        subseq_ascii(p, up.substring(1));
+        subseq_ascii(p + (ch+0), up.substring(1)); //ascii
+    }
+
+    static ArrayList<String> subseq_ascii_return(String p, String up){
+        if(up.isEmpty()){
+           ArrayList<String> list = new ArrayList<>();
+           list.add(p);
+           return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> with = subseq_ascii_return(p + ch, up.substring(1));
+        ArrayList<String> without = subseq_ascii_return(p, up.substring(1));
+        ArrayList<String> ascii = subseq_ascii_return(p + (ch+0), up.substring(1)); //ascii
+
+        with.addAll(without);
+        with.addAll(ascii);
+        return with;
+    }
+
+
+    // Iterative program to print subsequences
+    static List<List<Integer>> subset(int[] arr){ // Time - O( N * 2^N ) - Space
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+        for(int num : arr){
+            int n = outer.size();
+            for(int i = 0; i < n; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i)); //copy of existing list
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+
+    //subsequences of a string with duplicate elements
+    //The below algo will only work when duplicates are together => So sort the array first.
+
+    static List<List<Integer>> subset_duplicate(int[] arr){ // Time - O( N * 2^N ) - Space
+        Arrays.sort(arr); //it will update the original array.
+        
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            start = 0;
+            
+            if(i > 0 && arr[i] == arr[i-1]){
+                start = end + 1;
+            }
+
+            end = outer.size() - 1;
+            int n = outer.size();
+            
+            for(int j = start; j < n; j++){
+                List<Integer> internal = new ArrayList<>(outer.get(j)); //copy of existing list
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
     }
 }
