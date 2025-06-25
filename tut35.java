a4317c507dbdac98663e77ffe54c6b083d211ffc
Jay20241 <167299716+Jay20241@users.noreply.github.com>
2025-06-25 07:16:17 +0000

    Print Matrix and Paths, BackTrecking

--- tut35.java
+++ tut35.java
@@ -164,4 +164,42 @@ public class Maze{
     }
 
     // 6. Print Matrix and Paths
+    static void matrix_paths_with_allDirections(String p, boolean[][] maze, int r, int c, int[][] path, int step){
+        if(r == maze.length - 1 && c == maze[0].length - 1){
+            
+            path[r][c] = step; //last step reached
+
+            for(int[] arr: path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if(maze[r][c] == false){ //obstacle
+            return;
+        }
+
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        if(r < maze.length - 1){
+            matrix_paths_with_allDirections(p + 'D', maze, r+1, c, path, step + 1);
+        }
+        if(c < maze[0].length - 1){
+            matrix_paths_with_allDirections(p + 'R', maze, r, c+1, path, step + 1);
+        }
+
+        if(r > 0){
+            matrix_paths_with_allDirections(p + 'U', maze, r-1, c, path, step + 1);
+        }
+        if(c > 0){
+            matrix_paths_with_allDirections(p + 'L', maze, r, c-1, path, step + 1);
+        }
+        
+        maze[r][c] = true;
+        path[r][c] = 0;
+
+    }
 }
