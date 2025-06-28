c2c60f36f6ec1b0e2151fd1fe6b0df6033aa2c71
Jay20241 <167299716+Jay20241@users.noreply.github.com>
2025-06-28 13:29:05 +0000

    Singly LinkedList

---
+++ LinkedList/tut44.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+//see class LL
+public class Main{
+    psvm(){
+        //LinkedList<Integer> list = new LinkedList<>(); //Also use Generics
+        //list.add(15);
+
+        LL list = new LL();
+        //call any function form LL.java
+    }
+}
